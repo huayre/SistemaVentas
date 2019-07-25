5046e698435a3d7adb44a4c212cc3209d1b50083
unknown <roy.huayre@unas.edu.pe>
2019-07-25 09:40:34 +0200

    Modificando archivo database

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'http://165.227..88.102'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'sis-ventas'),
+            'database' => env('DB_DATABASE', 'sisventas'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', '/opt/lamp/var/mysql/mysql.sock'),
